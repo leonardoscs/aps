36d29edb7be84944b9b38ebc116d229487870ea9
root6439 <nicolas.soaresh.98@gmail.com>
2019-05-16 19:30:42 -0300

    Método para carregar uma view no programa

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -41,7 +50,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemCadastrarUsuario() {
-		System.out.println("a");
+		loadView("/gui/CadastrarUsuario.fxml");
 	}
 	
 	@FXML
@@ -88,10 +97,32 @@ public class MainViewController implements Initializable{
 	public void onMenuItemConsultaDeEmprestimo() {
 		System.out.println("h");
 	}
+	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		// TODO Auto-generated method stub
 		
+	}
+	
+	private synchronized void loadView(String caminhoDaView) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(caminhoDaView));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			
+			mainVBox.getChildren().add(mainMenu);
+			
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		
+		} catch (IOException e) {
+			Alerts.showAlert("Algo deu errado meu consagrado", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 }
