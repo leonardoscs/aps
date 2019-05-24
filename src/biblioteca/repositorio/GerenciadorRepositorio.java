c9c5e94e7b4d60f5b28efb6e9f3e20b3de0db3ee
leonardosncs <leonardosncs@gmail.com>
2019-05-24 10:35:43 -0300

    adiciona GerenciadorRepositorio

---
+++ src/biblioteca/repositorio/GerenciadorRepositorio.java
@@ -0,0 +1,33 @@
+package biblioteca.repositorio;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class GerenciadorRepositorio {
+
+  private Map<Class<?>, Repositorio<?>> repositorios = new HashMap<>();
+
+  public abstract void inicializar() throws Exception;
+  
+  @SuppressWarnings("unchecked")
+  public <T extends Repositorio<?>> T getRepositorio(Class<T> classe) {
+    Repositorio<?> repo = repositorios.get(classe);
+    if (repo == null) {
+      throw new RuntimeException(String.format("O repositorio '%s' não foi cadastrado.", 
+          classe.getSimpleName()));
+    }
+    return (T) repo;
+  }
+  
+  public void adicionarRepositorio(Repositorio<?> repo) {
+    // Cadastra pela classe da interface, não a da implementação.
+    // Por exemplo, RepositorioUsuario ao invés de RepositorioUsuarioSQL.
+    Class<?> classeRepositorio = Arrays.stream(repo.getClass().getInterfaces())
+        .filter(c -> Repositorio.class.isAssignableFrom(c))
+        .findFirst().get();
+    
+    repositorios.put(classeRepositorio, repo);
+  }
+
+}
---
+++ src/biblioteca/repositorio/sql/GerenciadorRepositorioSQL.java
@@ -0,0 +1,44 @@
+package biblioteca.repositorio.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import biblioteca.repositorio.GerenciadorRepositorio;
+import biblioteca.repositorio.RepositorioAutor;
+import biblioteca.repositorio.RepositorioCategoria;
+import biblioteca.repositorio.RepositorioEditora;
+import biblioteca.repositorio.RepositorioEmprestimo;
+import biblioteca.repositorio.RepositorioExemplarLivro;
+import biblioteca.repositorio.RepositorioLivro;
+import biblioteca.repositorio.RepositorioTipoUsuario;
+import biblioteca.repositorio.RepositorioUsuario;
+
+public class GerenciadorRepositorioSQL extends GerenciadorRepositorio {
+
+  @Override
+  public void inicializar() throws Exception {
+    // TODO: receber a conexão pelo constructor? sla
+    // TODO: adicionar opção para especificar url/senha/usuario do banco?
+    Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/biblioteca", "aps", "123");
+    
+    RepositorioAutor repoAutor = new RepositorioAutorSQL(conn);
+    RepositorioEditora repoEditora = new RepositorioEditoraSQL(conn);
+    RepositorioLivro repoLivro = new RepositorioLivroSQL(conn);
+    RepositorioUsuario repoUsuario = new RepositorioUsuarioSQL(conn);
+    RepositorioTipoUsuario repoTipo = new RepositorioTipoUsuarioSQL(conn);
+    RepositorioCategoria repoCat = new RepositorioCategoriaSQL(conn);
+    RepositorioExemplarLivro repoExemplar = new RepositorioExemplarLivroSQL(conn, repoLivro);
+    RepositorioEmprestimo repoEmp = new RepositorioEmprestimoSQL(conn, repoExemplar);
+    
+    adicionarRepositorio(repoAutor);
+    adicionarRepositorio(repoEditora);
+    adicionarRepositorio(repoUsuario);
+    adicionarRepositorio(repoTipo);
+    adicionarRepositorio(repoCat);
+    adicionarRepositorio(repoExemplar);
+    adicionarRepositorio(repoEmp);
+    
+    System.out.println(getRepositorio(RepositorioTipoUsuario.class));
+  }
+
+}
