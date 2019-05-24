cd978c349b095bab20aca36e7569dfee032677e8
leonardosncs <leonardosncs@gmail.com>
2019-05-24 10:37:05 -0300

    remove arquivos que foram duplicados em uma pasta diferente

--- biblioteca/repositorio/RepositorioUsuario.java
+++
@@ -1,8 +0,0 @@
-package biblioteca.repositorio;
-import biblioteca.entidades.Usuario;
-
-public interface RepositorioUsuario extends Repositorio<Usuario> {
-
-  Usuario buscarPelaMatricula(long matricula);
-  
-}
