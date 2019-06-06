091e70b6b419caed3aec5e8ca264541df7dc4e41
leonardosncs <leonardosncs@gmail.com>
2019-06-06 13:31:00 -0300

    remove código não utilizado

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -13,15 +13,6 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Utils {
-
-  public static Stage currentStage(ActionEvent event) {
-    return (Stage) ((Node)event.getSource()).getScene().getWindow();
-  }
-
-  public static Integer tryParseToInt(String str) {
-    return Integer.parseInt(str);
-  }
-
   // Limpa os campos dinamicamente usando reflection
   public static void limpaCamposDinamicamente(Object obj) {
     Field[] fields = obj.getClass().getDeclaredFields();
